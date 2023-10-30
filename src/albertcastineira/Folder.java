1040571c49b35fbb93fe2dadcfe5e31fcd90d1bd
albertcastineira <albertcastiaran@gmail.com>
2023-10-30 19:44:20 +0100

    add folder path functionality

--- src/albertcastineira/Folder.java
+++ src/albertcastineira/Folder.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 
 public class Folder {
     private String name;
+    private String path;
     private LocalDateTime lastModifiedTime;
     private String author = "admin";
     private int diskBlockCount = 4096;
     private int hardLinksCount = 1;
     private String permissions;
     private ArrayList<File> FILES = new ArrayList<>();
-    Folder(String folderName, String author, String hardLinksCount) {
+    Folder(String folderName, String path, String author, String hardLinksCount) {
         this.name = folderName;
         this.lastModifiedTime = LocalDateTime.now();
         if(author != null) this.author = author;
         if(hardLinksCount != null) this.hardLinksCount = Integer.parseInt(hardLinksCount);
         setDefaultPermissions();
+        this.path = path;
 
     }
 
@@ -26,28 +28,33 @@ public class Folder {
 
     // drwxr-xr-x 2 user user  4096 Oct 30 08:30 directory1
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getLastModifiedTime() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        return lastModifiedTime.format(formatter);
+        return this.lastModifiedTime.format(formatter);
     }
 
     public String getAuthor() {
-        return author;
+        return this.author;
     }
 
     public int getDiskBlockCount() {
-        return diskBlockCount;
+        return this.diskBlockCount;
     }
 
     public int getHardLinksCount() {
-        return hardLinksCount;
+        return this.hardLinksCount;
     }
 
 
     public String getPermissions() {
-        return permissions;
+        return this.permissions;
+    }
+
+    public String getPath() {
+        return this.path;
+
     }
 }
--- src/albertcastineira/Terminal.java
+++ src/albertcastineira/Terminal.java
@@ -6,18 +6,22 @@ import java.util.Scanner;
 public class Terminal {
 
     private String CURRENT_PATH = "~";
-    private String USER = "defaultUser";
-
+    private final String USER = "defaultUser";
+    private final int DEFAULT_CLEAR_LINES = 50;
     private final String ANSI_RESET = "\u001B[0m";
     private final String ANSI_BOLD = "\u001B[1m";
     private final String ANSI_RED = "\u001B[31m";
     private final String ANSI_GREEN = "\u001B[32m";
     private final String ANSI_YELLOW = "\u001B[33m";
     private final String ANSI_BLUE = "\u001B[34m";
-    private Scanner SCANNER = new Scanner(System.in);
+    private final Scanner SCANNER = new Scanner(System.in);
+    private static final Runtime.Version JAVA_VERSION = Runtime.version();
 
     private enum Command {
+        CLEAR("clear","Cleans all the text from the terminal."),
         LS("ls","Check files inside the current directory."),
+        CD("cd", "Go to a specific directory."),
+        PWD("pwd","Current directory"),
         LS_LA("ls -la","Show a detailed list of the files inside the current directory"),
         HELP("help","Display all the available commands."),
         EXIT("exit", "Exit the terminal."),
@@ -47,16 +51,25 @@ public class Terminal {
             this.dialogValue = dialogValue;
         }
     }
+    private enum FolderDialog {
+        NOT_FOUND("Folder not found");
+
+        private final String dialogValue;
+        FolderDialog(String dialogValue) {
+            this.dialogValue = dialogValue;
+        }
+    }
     private enum About {
-        NAME("[ Linux-Lite-Terminal ]"),
-        AUTHOR("Made by Albert Castiñeira\n");
+        NAME("-+-+-+-+-+-+-+-+-+-+-+-\nJAVA LINUX TERMINAL v0.1\n-+-+-+-+-+-+-+-+-+-+-+-\n"),
+        RUNNING_VERSION("Java Version: " + JAVA_VERSION),
+        AUTHOR("Made by Albert Castiñeira");
         private final String text;
         About(String text) {
             this.text = text;
         }
     }
-    private ArrayList<Folder> FOLDERS = new ArrayList<Folder>();
-
+    private final ArrayList<Folder> FOLDERS = new ArrayList<Folder>();
+    private Folder rootFolder = new Folder("bin",CURRENT_PATH,null,"1");
 
     Terminal() {
         initialize();
@@ -64,12 +77,21 @@ public class Terminal {
 
     private void initialize() {
         loadDefaultFolders();
+        about();
         waitForCommand();
     }
 
     private void loadDefaultFolders() {
-        Folder testFolder = new Folder("test", null, "1");
-        FOLDERS.add(testFolder);
+        createFolder("bin",CURRENT_PATH,null,"1");
+        createFolder("boot",CURRENT_PATH,null,"1");
+        createFolder("dev",CURRENT_PATH,null,"1");
+        createFolder("etc",CURRENT_PATH,null,"1");
+        createFolder("home",CURRENT_PATH,null,"1");
+        createFolder("lib",CURRENT_PATH,null,"1");
+        createFolder("media",CURRENT_PATH,null,"1");
+        createFolder("usr",CURRENT_PATH,null,"1");
+        createFolder("var",CURRENT_PATH,null,"1");
+        createFolder(USER,"~/usr",null,"1");
     }
 
     private void waitForCommand() {
@@ -81,6 +103,11 @@ public class Terminal {
 
     private void executeCommand(String commandInput) {
 
+        if(commandInput.contains("cd") && (commandSpaceCount(commandInput) == 1)) {
+            goToFolder(commandInput);
+            waitForCommand();
+        }
+
         Command userCommand = null;
         for (Command c : Command.values()) {
             if (c.getCommandValue().equals(commandInput)) {
@@ -91,12 +118,21 @@ public class Terminal {
 
         if (userCommand != null) {
             switch (userCommand) {
+                case CLEAR:
+                    clearTerminal();
+                    break;
                 case ABOUT:
                     about();
                     break;
                 case HELP:
                     print(ANSI_RESET + ANSI_BOLD + CommandDialog.HELP.dialogValue);
                     printAllCommands();
+                    break;
+                case PWD:
+                    showCurrentDIR();
+                    break;
+                case CD:
+                    goToRoot();
                     break;
                 case LS:
                     listFiles();
@@ -107,11 +143,40 @@ public class Terminal {
                 case EXIT:
                     System.exit(0);
                     break;
+                default:
+                    print(ANSI_RED + CommandDialog.NOT_FOUND.dialogValue);
+                    break;
             }
         } else {
             print(ANSI_RED + CommandDialog.NOT_FOUND.dialogValue);
         }
         waitForCommand();
+    }
+
+    private void goToRoot() {
+        CURRENT_PATH = "~";
+    }
+
+    private void showCurrentDIR() {
+        print(CURRENT_PATH);
+    }
+
+    private void goToFolder(String folderCommand) {
+        String[] split = folderCommand.split(" ");
+        String folderName = split[1];
+        Folder folder = null;
+        for (Folder folderToFind : FOLDERS) {
+            if (folderToFind.getName().equals(folderName)) {
+                folder = folderToFind;
+                break;
+            }
+        }
+        if (folder != null) {
+            CURRENT_PATH += "/" + folder.getName();
+        } else {
+            print(FolderDialog.NOT_FOUND.dialogValue);
+        }
+
     }
 
     private void print(String line) {
@@ -120,6 +185,7 @@ public class Terminal {
 
     private void about() {
         print(ANSI_BOLD + About.NAME.text + ANSI_RESET);
+        print(ANSI_BOLD + About.RUNNING_VERSION.text);
         print(About.AUTHOR.text);
     }
 
@@ -131,9 +197,23 @@ public class Terminal {
 
     private void listFiles() {
         for(Folder folder : FOLDERS) {
-            print(folder.getName());
+            if(folder.getPath().equals(CURRENT_PATH)) {
+                print(folder.getName());
+            }
         }
     }
+
+    private int commandSpaceCount(String command) {
+        int spaceCount = 0;
+
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == ' ') {
+                spaceCount++;
+            }
+        }
+        return spaceCount;
+    }
+
     private void listDetailedFiles() {
         print("total " + FOLDERS.size());
         for(Folder folder : FOLDERS) {
@@ -143,5 +223,20 @@ public class Terminal {
                 folder.getName()
             );
         }
+    }
+
+    private void clearTerminal() {
+        for (int i = 0; i < DEFAULT_CLEAR_LINES; i++) {
+            System.out.println();
+        }
+    }
+
+    private void createFolder(String folderName, String folderPath, String author, String hardLinksCount) {
+        Folder folder = new Folder(folderName, folderPath, author, hardLinksCount);
+        FOLDERS.add(folder);
+    }
+
+    private void createFile() {
+        // TODO: Create basic file functionality
     }
 }
